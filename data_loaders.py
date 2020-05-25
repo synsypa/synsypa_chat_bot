47f4d2fe3584096399fd735f6685e32832757c56
synsypa <changjiang23@gmail.com>
2020-05-25 23:45:00 +0000

    refactored data loading

--- data_loaders.py
+++ data_loaders.py
@@ -6,9 +6,9 @@ import torch
 from torch.utils.data import Dataset
 from torchtext import vocab
 
-SOS_TOKEN = "<sos>"
-EOS_TOKEN = "<end>"
 PAD_TOKEN = "<pad>"
+SOS_TOKEN = "<sos>"
+EOS_TOKEN = "<eos>"
 UNK_TOKEN = "<unk>"
 
 def create_vocab(convos):
@@ -18,18 +18,17 @@ def create_vocab(convos):
     counter = Counter(' '.join(flat).split())
 
     voc = vocab.Vocab(counter, min_freq=3,
-                      specials=[SOS_TOKEN, EOS_TOKEN, PAD_TOKEN, UNK_TOKEN])
+                      specials=[PAD_TOKEN, SOS_TOKEN, EOS_TOKEN, UNK_TOKEN])
     
     return voc
 
 class ConvoDataset(Dataset):
     """ Dataset class of conversations """
 
-    def __init__(self, convos, vocab, max_size):
+    def __init__(self, convos, vocab):
 
         self.convos = convos
         self.vocab = vocab
-        self.max_size = max_size
 
     def __len__(self):
 
@@ -39,13 +38,12 @@ class ConvoDataset(Dataset):
 
         call, response = self.convos[idx]
 
-        call_tensor = self._vectorize(call)
-        response_tensor = self._vectorize(response)
+        call_tensor = self._to_index(call)
+        response_tensor = self._to_index(response)
 
-        return {'call': call_tensor,
-                'response': response_tensor} 
+        return call_tensor, response_tensor
 
-    def _vectorize(self, string):
+    def _to_index(self, string):
         
         tokens = string.split()
         tokens.append(EOS_TOKEN)
@@ -55,27 +53,29 @@ class ConvoDataset(Dataset):
                     else self.vocab.stoi[UNK_TOKEN]
                     for word in tokens]
 
-        text_dim = min(len(token_idx), self.max_size)
-        text_tensor_padded = torch.zeros(self.max_size, dtype = torch.long)
-        text_tensor_padded[ :text_dim] = torch.tensor(token_idx[ :text_dim])
+        return torch.tensor(token_idx)
 
-        if text_dim < (self.max_size - 1):
-            text_tensor_padded[text_dim: ] = self.vocab.stoi[PAD_TOKEN]
+def pad_collate(batch):
+    (xx, yy) = zip(*batch)
+    x_lens = [len(x) for x in xx]
+    y_lens = [len(y) for y in yy]
 
-        return text_tensor_padded  
+    xx_pad = pad_sequence(xx, batch_first=True, padding_value=0)
+    yy_pad = pad_sequence(yy, batch_first=True, padding_value=0)
 
+    return xx_pad, yy_pad, x_lens, y_lens
 
-
-
-
+        
 if __name__ == "__main__":
     convos = pickle.load(open('chat_data/clean_conversations_2020-05-25.pkl', 'rb'))
 
     vocab = create_vocab(convos)
 
-    test = ConvoDataset(convos, vocab, 10)
+    test = ConvoDataset(convos, vocab)
 
     print(test[0])
+    print(type(test[0]))
     print('\n')
-    print([vocab.itos[i] for i in test[0]['call']])
-    print([vocab.itos[i] for i in test[0]['response']])
+    print([vocab.itos[i] for i in test[0][0]])
+    print([vocab.itos[i] for i in test[0][1]])
+
